6c1154df04aa4fe339fbf7a2e2199bf8ee80ee78
aniketsingh6712 <124377947+aniketsingh6712@users.noreply.github.com>
2025-03-30 14:22:20 +0530

    Update user_appointment.jsx

--- frontend/src/appointment/doctor/user_appointment.jsx
+++ frontend/src/appointment/doctor/user_appointment.jsx
@@ -4,11 +4,12 @@ import React, { useState,useEffect } from 'react'
 import { auth } from '../../firebase/firebase-config';
 import AppointmentTable from './AppointmentTable';
 import { HeroSection } from '../../component/Home/Home';
+const API_URL = import.meta.env.VITE_API_URL;
 function Doc_Appointment() {
     const [appointmentData,setAppointmentData]=useState([]);
     const getAppointmentinfo=async ()=>{
         try {
-          const response = await fetch(`http://localhost:3001/appointment-record/fetch/appointment-record/doctor/${auth?.currentUser?.uid}`, {
+          const response = await fetch(`${API_URL}/appointment-record/fetch/appointment-record/doctor/${auth?.currentUser?.uid}`, {
             method: "GET", // Change to GET
             headers: {
               "Content-Type": "application/json",
